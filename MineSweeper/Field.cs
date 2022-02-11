83d70dfc98f0fecc33aafbaf5d95bd3586dc21fa
Dmitry Alexeev <dalexeev.work@gmail.com>
2022-02-11 17:11:09 +0300

    Add cell changing.

--- MineSweeper/Field.cs
+++ MineSweeper/Field.cs
@@ -10,10 +10,32 @@ namespace MineSweeper
     {
         static readonly int N = 9;
         int[,] cells = new int[N, N];
-        
+        enum state
+        {
+            Opened,
+            Closed,
+            Flag,
+            Bomb
+        }
+        state[,] cells_prop = new state[N, N];
+
         //public Field()
         //{  
         //}
+
+        private void GenerateCellsProperty()
+        {
+            int rows = cells_prop.GetUpperBound(0) + 1;
+            int column = cells_prop.Length / rows;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    cells_prop[i, j] = state.Closed;
+                }
+            }
+        }
 
         public void GenerateDigits()
         {
@@ -43,6 +65,7 @@ namespace MineSweeper
                     }    
                 }
             }
+            GenerateCellsProperty();
         }
 
         public void GenerateBombs(int qnt)
@@ -83,9 +106,38 @@ namespace MineSweeper
             {
                 for (int j = 0; j < column; j++)
                 {
-                    Console.Write("#\t");
+                    //Console.Write("#\t");
+                    switch (cells_prop[i, j])
+                    {
+                        case state.Opened:
+                            Console.Write($"{cells[i, j]}\t");
+                            break;
+                        case state.Closed:
+                            Console.Write($"#\t");
+                            break;
+                        case state.Flag:
+                            Console.Write("!\t");
+                            break;
+                        case state.Bomb:
+                            Console.Write("*\t");
+                            break;
+
+                    }
                 }
                 Console.WriteLine();
+            }
+        }
+
+        public void ChangeCellState(int x, int y, int input)
+        {
+            if (cells[x, y] == -1 && input == 1)
+            {
+                cells_prop[x, y] = state.Bomb;
+            }
+            else
+            {
+                if (input == 2) cells_prop[x, y] = state.Flag;
+                if (input == 1) cells_prop[x, y] = state.Opened;
             }
         }
     }
--- MineSweeper/Program.cs
+++ MineSweeper/Program.cs
@@ -8,16 +8,45 @@ namespace MineSweeper
     {
         public static void Main(string[] args)
         {
+            int input;
+            Field field = new Field();
+
+
             Console.WriteLine("Welcome to Minesweeper!");
             
-            Field field = new Field();
             field.GenerateBombs(10);
             field.GenerateDigits();
             Console.WriteLine("Ready to play!");
+            field.ShowField();
 
             // Cycle for playing
             while (true)
             {
+                Console.WriteLine("Choose action:\n1)Choose cell\n2)Exit");
+                Console.Write("Input:\t");
+                input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Input coordinates");
+                        int x, y;
+                        Console.Write("x = \t");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("y = \t");
+                        y = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Choose action:\n1)Open cell\n2)Put flag");
+                        Console.Write("Input:\t");
+                        input = Convert.ToInt32(Console.ReadLine());
+                        field.ChangeCellState(x-1, y-1, input);
+                        field.ShowField();
+                        break;
+                    case 2:
+                        Console.WriteLine("Bye!");
+                        return;
+                    default:
+                        break;
+                }
             }
 
         }
