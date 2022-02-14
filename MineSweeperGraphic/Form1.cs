790b275fc21b3c5341a8d6149c9c98fe827846dc
Dmitry Alexeev <dalexeev.work@gmail.com>
2022-02-14 16:48:10 +0300

    Create new project.

---
+++ MineSweeperGraphic/Form1.cs
@@ -0,0 +1,17 @@
+namespace MineSweeperGraphic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Click += button1_Click;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+    }
+}
