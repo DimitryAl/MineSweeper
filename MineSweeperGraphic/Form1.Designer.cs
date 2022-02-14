1142f25332907efdff3b6bfcada5bf9225e9b3b3
Dmitry Alexeev <dalexeev.work@gmail.com>
2022-02-14 19:09:23 +0300

    Added stopwatch, buttons "new game" and "leaderboard".

--- MineSweeperGraphic/Form1.Designer.cs
+++ MineSweeperGraphic/Form1.Designer.cs
@@ -28,7 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button1
@@ -41,20 +45,49 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Leaderboard";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MineSweeper";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button button1;
+        private Button button2;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- MineSweeperGraphic/Form1.cs
+++ MineSweeperGraphic/Form1.cs
@@ -2,16 +2,58 @@ namespace MineSweeperGraphic
 {
     public partial class Form1 : Form
     {
+        System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+
         public Form1()
         {
             InitializeComponent();
+            
+            UpdateTime();
 
             button1.Click += button1_Click;
         }
 
+        private void UpdateTime()
+        {
+            label1.Text = GetTimeString(stopWatch.Elapsed);
+        }
+
+        private string GetTimeString(TimeSpan elapsed)
+        {
+            string timeString = string.Empty;
+
+            timeString = string.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+                elapsed.Hours,
+                elapsed.Minutes,
+                elapsed.Seconds,
+                elapsed.Milliseconds
+                );
+
+            return timeString;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Test");
+            timer1.Enabled = true;
+
+            stopWatch.Start();
+
+            //MessageBox.Show("btn1");
+            
+            //label1.Text = stopWatch.Elapsed.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("btn2");
+
+            Form2 leaderboard1 = new Form2();
+            leaderboard1.Show();
+        }
+
+        private void timer1_tick(object sender, EventArgs e)
+        {
+            UpdateTime();
         }
     }
 }
